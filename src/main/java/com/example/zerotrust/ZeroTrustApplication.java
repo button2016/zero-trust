15a1d4e085c086b8e8af847753213030ad3aae4e
button <2933276056@qq.com>
2025-02-16 20:52:09 +0800

    initialize

---
+++ src/main/java/com/example/zerotrust/ZeroTrustApplication.java
@@ -0,0 +1,13 @@
+package com.example.zerotrust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroTrustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroTrustApplication.class, args);
+    }
+
+}
